cc8929dd9aae3c074b95acd4c9565778f0c9d844
Kushal Gupta <kushalgupta0106@gmail.com>
2021-09-04 01:18:47 +0530

    added device identification code

--- knowyourkidz/src/app/app.component.ts
+++ knowyourkidz/src/app/app.component.ts
@@ -7,8 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'knowyourkidz';
+device='';
+isMobile:boolean=false;
+  constructor(){
+
+    if(this.detectMob()){
+      this.isMobile=true;
+      this.device='Open from Mobile';
+    }else{
+      this.isMobile=false;
+      this.device='Open from Desktop';
+    }
+  }
 
   start(){
     console.log("Fsd")
   }
+
+   detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
 }
--- knowyourkidz/src/app/zoom-integration/zoom-integration.component.ts
+++ knowyourkidz/src/app/zoom-integration/zoom-integration.component.ts
@@ -30,6 +30,7 @@ export class ZoomIntegrationComponent implements OnInit{
   }
 
   
+  
   start(){
     this.signature=ZoomMtg.generateSignature({
       meetingNumber:"88569336349",
@@ -142,6 +143,22 @@ export class ZoomIntegrationComponent implements OnInit{
         console.log(error)
       }
     })
+  }
+
+  detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
   }
 
 }
